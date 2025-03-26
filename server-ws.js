cef05381ed3b20dc66f4a40b1a7a6e274e46bc1c
Luis Octávio da Silva Carvalho <luisoctavius.sc@gmail.com>
2025-03-26 17:54:26 +0000

    feat: websocket

--- server-ws.js
+++ server-ws.js
@@ -10,4 +10,35 @@ app.get('/', function(req, res) {
 server.on('request', app);
 server.listen(3000, function () { console.log('Listening on 3000'); });
 
+// Websocket
+const wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+	const numClients = wss.clients.size;
+
+	console.log('clients connected: ', numClients);
+
+	wss.broadcast(`Current visitors: ${numClients}`);
+
+	if (ws.readyState === ws.OPEN) {
+		ws.send('welcome!');
+	}
+
+	ws.on('close', function close() {
+		wss.broadcast(`Current visitors: ${wss.clients.size}`);
+		console.log('A client has disconnected');
+	});
+
+	ws.on('error', function error() {
+
+	});
+});
+
+wss.boardcast = function broadcast(data) {
+	console.log('Broadcasting: ', data);
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	});
+};
+
 
